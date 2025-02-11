eac2415a95e5596422ebdffcabe5a1ce8e721ba6
Desktop4090 <zhangqiart@outlook>
2025-02-12 00:50:31 +0800

    添加Azure语音合成

--- index.js
+++ index.js
@@ -65,63 +65,133 @@ function run(){
 			const text = req.body.text;
 			const trackSelect = req.body.trackSelect;
 			const voiceSelect = req.body.voiceSelect;
-	
-			console.log("Parameters:", { text, folderPath, trackSelect });
-	
-			const params = {
-				token: 'ttsmaker_demo_token',
-				text: text,
-				voice_id: voiceSelect,
-				audio_format: 'mp3',
-				audio_speed: 1.0,
-				audio_volume: 0,
-				text_paragraph_pause_time: 0
-			};
-	
-			console.log("Sending request to TTS API");
-			const response = await axios.post('https://api.ttsmaker.cn/v1/create-tts-order', params);
-			console.log("Received response from TTS API:", response.data);
-	
-			const audio_file_url = response.data.audio_file_url;
-			console.log('audio_file_url:', audio_file_url);
+			const platformSelect = req.body.platformSelect;
 
-			if(!audio_file_url){
-				error_code = response.data.error_code;
-				if(error_code == "TOTAL_TOKEN_CHARACTERS_EXCEED_LIMIT"){
-					res.status(200).send("音频下载遇到错误: 本周Token余额已用完");
-				}else{
-					res.status(200).send("音频下载遇到错误: " + response.data.error_code);
-				}
+			//如果text为空
+			if(!text){
+				res.status(200).send("未输入文本");
 				return;
 			}
 	
-			const childFolderPath = path.resolve(folderPath, 'TTS Audio');
-			if (!fs.existsSync(childFolderPath)) {
-				console.log("Creating directory:", childFolderPath);
-				fs.mkdirSync(childFolderPath);
-			}
+			console.log("Parameters:", { text, folderPath, trackSelect, voiceSelect, platformSelect });
 	
-			let fileName = text.replace(/[^\w\s\u4e00-\u9fa5]/gi, '').trim();
-			fileName = fileName.substring(0, 30);
-			//移除换行
-			fileName = fileName.replace(/[\r\n]/g, '');
-			fileName += new Date().getTime();
-			fileName += '.mp3';
-	
-			const outputLocationPath = path.resolve(childFolderPath, fileName);
-			console.log('outputLocationPath:', outputLocationPath);
-	
-			console.log("Starting download of MP3");
-			await downloadMp3(audio_file_url, outputLocationPath);
-			console.log("Download completed");
-			
+			if(platformSelect == "ttsmaker"){
+				const params = {
+					token: 'ttsmaker_demo_token',
+					text: text,
+					voice_id: voiceSelect,
+					audio_format: 'mp3',
+					audio_speed: 1.0,
+					audio_volume: 0,
+					text_paragraph_pause_time: 0
+				};
+		
+				console.log("Sending request to TTS API");
+				const response = await axios.post('https://api.ttsmaker.cn/v1/create-tts-order', params);
+				console.log("Received response from TTS API:", response.data);
+		
+				const audio_file_url = response.data.audio_file_url;
+				console.log('audio_file_url:', audio_file_url);
 
-			const filePath = outputLocationPath.replace(/\\/g, "/");
-					console.log("downloadAudio filePath: " + filePath);
-					csInterface.evalScript(`$._ext.importSingleFile("${filePath}", ${trackSelect})`, function(result) {
-                        console.log(result);
-						res.status(200).send(result);
-                    });
+				if(!audio_file_url){
+					error_code = response.data.error_code;
+					if(error_code == "TOTAL_TOKEN_CHARACTERS_EXCEED_LIMIT"){
+						res.status(200).send("TTSMaker 本周 Token 已用完，请选择其他平台");
+					}else{
+						res.status(200).send("音频下载遇到错误: " + response.data.error_code);
+					}
+					return;
+				}
+		
+				const childFolderPath = path.resolve(folderPath, 'TTS Audio');
+				if (!fs.existsSync(childFolderPath)) {
+					console.log("Creating directory:", childFolderPath);
+					fs.mkdirSync(childFolderPath);
+				}
+		
+				let fileName = text.replace(/[^\w\s\u4e00-\u9fa5]/gi, '').trim();
+				fileName = fileName.substring(0, 30);
+				//移除换行
+				fileName = fileName.replace(/[\r\n]/g, '');
+				fileName += new Date().getTime();
+				fileName += '.mp3';
+		
+				const outputLocationPath = path.resolve(childFolderPath, fileName);
+				console.log('outputLocationPath:', outputLocationPath);
+		
+				console.log("Starting download of MP3");
+				await downloadMp3(audio_file_url, outputLocationPath);
+				console.log("Download completed");
+				
+
+				const filePath = outputLocationPath.replace(/\\/g, "/");
+						console.log("downloadAudio filePath: " + filePath);
+						csInterface.evalScript(`$._ext.importSingleFile("${filePath}", ${trackSelect})`, function(result) {
+							console.log(result);
+							res.status(200).send(result);
+						});
+
+
+			}else if(platformSelect == "azure"){
+				const childFolderPath = path.resolve(folderPath, 'TTS Audio');
+				if (!fs.existsSync(childFolderPath)) {
+					console.log("Creating directory:", childFolderPath);
+					fs.mkdirSync(childFolderPath);
+				}
+		
+				let fileName = text.replace(/[^\w\s\u4e00-\u9fa5]/gi, '').trim();
+				fileName = fileName.substring(0, 30);
+				//移除换行
+				fileName = fileName.replace(/[\r\n]/g, '');
+				fileName += new Date().getTime();
+				fileName += '.mp3';
+		
+				const outputLocationPath = path.resolve(childFolderPath, fileName);
+				console.log('outputLocationPath:', outputLocationPath);
+
+
+
+				//使用Azure js sdk
+				var sdk = require("microsoft-cognitiveservices-speech-sdk");
+
+				var audioFile = outputLocationPath;
+
+				const speechConfig = sdk.SpeechConfig.fromSubscription("", "eastasia");
+				const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+
+				// The language of the voice that speaks.
+				speechConfig.speechSynthesisVoiceName = voiceSelect; 
+
+				const synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+				//合成语音并下载到指定目录
+				synthesizer.speakTextAsync(
+					text,
+					function (result) {
+						if (result) {
+							console.log(JSON.stringify(result));
+							console.log("Speech synthesized to speaker for text [" + text + "]");
+
+							const filePath = outputLocationPath.replace(/\\/g, "/");
+							console.log("downloadAudio filePath: " + filePath);
+							csInterface.evalScript(`$._ext.importSingleFile("${filePath}", ${trackSelect})`, function(result) {
+								console.log(result);
+								res.status(200).send(result);
+							});
+
+						} else {
+							console.log("Unexpected synthesis result: " + result);
+							res.status(200).send("音频下载遇到错误: " + result);
+						}
+					},
+					function (err) {
+						console.trace("err - " + err);
+						res.status(200).send("音频下载遇到错误: " + err);
+					}
+				);
+
+			}
+
 		
 		} catch (error) {
 			console.log("Error occurred:", error);
